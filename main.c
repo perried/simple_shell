a54b914f8e3b2c8a214173712172aa91b183788a
Perrie <periedamas203@gmail.com>
2023-12-12 14:26:36 -0800

    Done with READ & EXECUTE in a shell

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Main function of the program
+ *
+ * Return: returns 0 if successful
+ */
+int main(void)
+{
+	size_t bufsize = 0;
+	char *buffer = NULL;
+	const char *delim = "\n\t\r";
+	char *token;
+	char *argv[2];
+	char prompt[] = {'>', '>', '>', ' '};
+
+	write(1, &prompt, 4);
+	getline(&buffer, &bufsize, stdin);
+
+	token = strtok(buffer, delim);
+	argv[0] = token;
+	argv[1] = NULL;
+
+	if (execve(argv[0], argv, environ) == -1)
+		perror("hsh");
+
+	free(buffer);
+
+	return (0);
+}
