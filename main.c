39d1c76359d6d32cbb0f00280a68e5ebceebbb43
Perrie <periedamas203@gmail.com>
2023-12-19 12:08:29 -0800

    Fixing unused argument error

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
  * main - Main function of the program
  * @argc: argument count
  * @argv: argument vector
- *
  * Return: returns 0 if successful
  */
 int main(int argc, char *argv[])
@@ -15,19 +14,26 @@ int main(int argc, char *argv[])
 	const char *delim = "\n\t\r";
 	char *token;
 	char *av[2];
-	int status;
+	ssize_t status;
 
+	(void) argc;
 	while (1)
 	{
 
 		write(1, &"#cisfun$ ", 10);
 		status = getline(&buffer, &bufsize, stdin);
-		if (status == -1 && status == EOF)
+		if (status == -1 || status == EOF)
 			return (-1);
 
 		token = strtok(buffer, delim);
-		av[0] = token;
-		av[1] = NULL;
+
+		if (token != NULL || token != " ")
+		{
+			av[0] = token;
+			av[1] = NULL;
+		}
+		else
+			continue;
 
 		pid = fork();
 
